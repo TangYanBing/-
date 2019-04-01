d57817f25f1b15361017fce7843e3a1ac27d107c
tyb <tyb@doll.com>
2019-04-01 18:38:27 +0800

    测试

--- dollMachineTask/src/test/java/com/wwj/task/TaskApplicationTests.java
+++ dollMachineTask/src/test/java/com/wwj/task/TaskApplicationTests.java
@@ -1,7 +1,11 @@
 package com.wwj.task;
 
+import com.wwj.task.beans.po.DemoUserPo;
+import com.wwj.task.repository.DemoRepository;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TaskApplicationTests {
 
+    @Autowired
+    DemoRepository demoRepository;
     @Test
     public void contextLoads() {
+        DemoUserPo userInfo = demoRepository.findUserInfo("20003848");
+        System.out.println(userInfo);
     }
 
 }
